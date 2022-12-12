cb54e8e076abaafd223ee232834b102a0f5a0aad
Artem <kuzmichev8-1991@list.ru>
2022-12-12 11:58:34 +0300

    add Example 006 Giri

---
+++ Example006_Giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int A = 1;
+int B = 3;
+int C = 5;
+int D = 8;
+int E = 4;
+
+int max = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+if (D > max) max = D;
+if (E > max) max = E;
+
+Console.Write("max = ");
+Console.WriteLine(max);
