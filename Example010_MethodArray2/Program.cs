de597560aee88a2800252de1b169c237fdab8124
Artem <kuzmichev8-1991@list.ru>
2022-12-13 17:06:10 +0300

    function 3

---
+++ Example010_MethodArray2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array ={11, 21, 31, 42, 12, 55, 34, 22, 555, 12};
+
+int n = array.Length;
+int find = 12;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index ++;
+}
